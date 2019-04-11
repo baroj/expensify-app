eb381ac2e32de1a95db915ce873666dc2ab8a5fe
Johnattan Barona <johnattanbarona@gmail.com>
2019-04-11 09:22:42 -0400

    Firebase Database No Keys

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import ExpenserForm from './ExpenseForm';
-import {connect} from 'react-redux';
-import {addExpense} from '../actions/expenses';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { startAddExpense } from '../actions/expenses';
 
-
-export class AddExpense extends React.Component {
-    onSubmit = (expense) => {
-        this.props.addExpense(expense);
-        this.props.history.push('/');
-    };
-
-    render() {
-        return (
-            <div>
-                <h1>AddExpense</h1>
-                <ExpenserForm
-                    onSubmit={this.onSubmit}
-                />
-            </div>
-            );
-    }
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.startAddExpense(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
 }
 
-
 const mapDispatchToProps = (dispatch) => ({
-        addExpense: (expense)=>dispatch(addExpense(expense))
+  startAddExpense: (expense) => dispatch(startAddExpense(expense))
 });
 
-export default connect(undefined, mapDispatchToProps)(AddExpense);
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -20,6 +20,8 @@ const expensesReducer = (state= expensesReducerDefState, action) => {
                     return expense;
                 }
             });
+        case 'SET_EXPENSES':
+            return action.expenses;          
         default:
             return state;
     }

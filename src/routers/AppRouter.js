295fff845e0e2089313078a88a84ab01ec7cfeb4
Johnattan Barona <johnattanbarona@gmail.com>
2019-04-13 15:19:03 -0400

    Public Router, User data saved with uid

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,6 +8,7 @@ import LoginPage from '../components/LoginPage';
 import HelpPage from '../components/Help';
 import NotFoundPage from '../components/NotFound';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -16,7 +17,7 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-            <Route path="/" component={LoginPage} exact={true}/>
+            <PublicRoute path="/" component={LoginPage} exact={true}/>
             <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/> 
             <PrivateRoute path="/create" component={AddExpense}/>
             <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
